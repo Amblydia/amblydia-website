59df57ffd87000e0a790efb85f4b6f8bd97a3a9b
JeanTKG <102908437+JeanTKG@users.noreply.github.com>
2024-03-07 17:10:43 +0300

    config change

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'http',
+				hostname: '164.68.103.165',
+				port: '1337',
+				pathname: '/uploads/**',
+			},
+			{
+				protocol: 'http',
 				hostname: 'localhost',
 				port: '1337',
 				pathname: '/uploads/**',
